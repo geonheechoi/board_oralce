3af1e3979cba48d2bd7a3e2e5c7019bbbf01437c
geonheechoi <58959798+geonheechoi@users.noreply.github.com>
2023-10-09 14:20:57 +0900

    Update conn_cloud.php

--- include/conn_cloud.php
+++ include/conn_cloud.php
@@ -1,4 +1,4 @@
 <?php
 //=============DB ���� ========================
-$conn = oci_connect('pds', 'koreapds3337', 'ORA_CLOUD', 'KO16MSWIN949') or die("[ON_LINE_01]ORACLE DATABASE Server�� ������ �� �����ϴ�.");
+$conn = oci_connect() or die("[ON_LINE_01]ORACLE DATABASE Server�� ������ �� �����ϴ�.");
 ?>
